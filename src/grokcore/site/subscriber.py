3c270ffaa7a28a443be80434a8b3b1f9f646ab92
Sylvain Viollon <sviollon@minddistrict.com>
2016-09-14 14:20:51 +0200

    When removing a site make sure we remove all references to it.

--- src/grokcore/site/subscriber.py
+++ src/grokcore/site/subscriber.py
@@ -13,10 +13,13 @@
 ##############################################################################
 
 from zope.lifecycleevent.interfaces import IObjectAddedEvent
+from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 from zope.site import LocalSiteManager
+from zope.site.site import _findNextSiteManager
 
 import grokcore.component
 from grokcore.site.components import Site
+
 
 @grokcore.component.subscribe(Site, IObjectAddedEvent)
 def addSiteHandler(site, event):
@@ -25,12 +28,24 @@ def addSiteHandler(site, event):
     Grok registers this function so that it gets called each time a
     `grokcore.site.Site` instance is added to a container.  It creates
     a local site manager and installs it on the newly created site.
-
     """
-
+    if event.oldParent is not None:
+        # We were moving the site.
+        return
     sitemanager = LocalSiteManager(site)
     # LocalSiteManager creates the 'default' folder in its __init__.
     # It's not needed anymore in new versions of Zope 3, therefore we
     # remove it
     del sitemanager['default']
     site.setSiteManager(sitemanager)
+
+
+@grokcore.component.subscribe(Site, IObjectRemovedEvent)
+def removeSiteHandler(site, event):
+    if event.newParent is not None:
+        # We were moving the site.
+        return
+    local = site.getSiteManager()
+    parent = _findNextSiteManager(site)
+    if parent is not None:
+        parent.removeSub(local)
