ea82d7cbdcc76d0bd712d02647956c02cb17c86e
Sylvain Viollon <sviollon@minddistrict.com>
2016-09-21 11:33:27 +0200

    Preparing release 1.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grokcore.site',
-    version='1.8.dev0',
+    version='1.8',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
