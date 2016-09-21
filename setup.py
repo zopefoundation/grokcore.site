01c9816cec6af6785a5060e21b56b1d1dd1d9860
Sylvain Viollon <sviollon@minddistrict.com>
2016-09-21 11:33:48 +0200

    Back to development: 1.9

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grokcore.site',
-    version='1.8',
+    version='1.9.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
